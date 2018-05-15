69bb3c4ed764c79e2bffb3c31a31a102ece3232e
Marcel Kunze <marcel.kunze@urz.uni-heidelberg.de>
2018-05-15 15:09:51 +0200

    V1.1

--- macro/writehits.C
+++ macro/writehits.C
@@ -37,18 +37,23 @@
         treereco->GetEntry(evt);
         cout << "Event " << evt << ": MC Hits " << digi->GetEntriesFast() << endl;
  
-        string name = "digi" + to_string(evt);
-        ofstream fdigi(name);
+        string name = "mct" + to_string(evt);
+        ofstream fmct(name);
         for (Int_t m = 0; m < digi->GetEntriesFast(); m++) {
             PndSttPoint *mcpoint = (PndSttPoint*) pnt->At(m);
             if (mcpoint==0) continue;
             cout << m << ": x " << mcpoint->GetX() << " y " << mcpoint->GetY() << " z " << mcpoint->GetZ() << endl;
-            fdigi << mcpoint->GetX() << "   " << mcpoint->GetY() << "   " << mcpoint->GetZ() << endl;
+            fmct << mcpoint->GetX() << "   " << mcpoint->GetY() << "   " << mcpoint->GetZ() << endl;
         }
-        fdigi.close();
+        fmct.close();
+        
+        string simname = "sim" + to_string(evt);
+        ofstream fsim(simname);
+        string diginame = "digi" + to_string(evt);
+        ofstream fdigi(diginame);
         
         cout << "Event " << evt << ": num tracks " << track->GetEntriesFast() << endl;
-
+        
         // tracks loop
         for (Int_t k = 0; k < track->GetEntriesFast(); k++) {
             
@@ -59,18 +64,22 @@
             int mcid = stttrack->getMcTrackId();
             cout << "McTrackId " << mcid << endl;
             
-           for (Int_t i = 0; i < stttrack->GetNHits(); i++) {
-               PndTrackCandHit h = stttrack->GetSortedHit(i);
-               Int_t hitindex = h.GetHitId();
-               cout << " " << hitindex;
-               PndSttHit* hit = (PndSttHit*) digi->At(hitindex);
-	       if (hit==0) continue;
-               cout << " x " << hit->GetX() << " y " << hit->GetY() << " z " << hit->GetZ();
-               PndSttPoint *mcpoint = (PndSttPoint*) pnt->At(hit->GetRefIndex());
-	       if (mcpoint==0) continue;
-               cout << "  MC: x " << mcpoint->GetX() << " y " << mcpoint->GetY() << " z " << mcpoint->GetZ() << endl;
-           }
+            for (Int_t i = 0; i < stttrack->GetNHits(); i++) {
+                PndTrackCandHit h = stttrack->GetSortedHit(i);
+                Int_t hitindex = h.GetHitId();
+                cout << " " << hitindex;
+                PndSttHit* hit = (PndSttHit*) digi->At(hitindex);
+                if (hit==0) continue;
+                cout << " x " << hit->GetX() << " y " << hit->GetY() << " z " << hit->GetZ();
+                fdigi << hit->GetX() << "   " << hit->GetY() << "   " << hit->GetZ() << endl;
+                PndSttPoint *mcpoint = (PndSttPoint*) pnt->At(hit->GetRefIndex());
+                if (mcpoint==0) continue;
+                cout << "  MC: x " << mcpoint->GetX() << " y " << mcpoint->GetY() << " z " << mcpoint->GetZ() << endl;
+                fsim << mcpoint->GetX() << "   " << mcpoint->GetY() << "   " << mcpoint->GetZ() << endl;
+            }
             cout << endl;
+            fsim.close();
+            fdigi.close();
         }
     }
 }
