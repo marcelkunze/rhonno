8020f116bf61fbbf556f075ba9bc2b60e09f9c89
Marcel Kunze <marcel@ubuntu18.fritz.box>
2019-01-29 12:36:45 +0000

    training

---
+++ RhoNNO/train20.sh
@@ -0,0 +1,17 @@
+../bin/NetworkTrainer trackml20.nno
+cp ../Networks/NNO0000.TXMLP XMLP20.net
+../bin/NetworkTrainer trackml21.nno
+cp ../Networks/NNO0000.TXMLP XMLP21.net
+../bin/NetworkTrainer trackml22.nno
+cp ../Networks/NNO0000.TXMLP XMLP22.net
+../bin/NetworkTrainer trackml23.nno
+cp ../Networks/NNO0000.TXMLP XMLP23.net
+../bin/NetworkTrainer trackml24.nno
+cp ../Networks/NNO0000.TXMLP XMLP24.net
+../bin/NetworkTrainer trackml25.nno
+cp ../Networks/NNO0000.TXMLP XMLP25.net
+../bin/NetworkTrainer trackml26.nno
+cp ../Networks/NNO0000.TXMLP XMLP26.net
+../bin/NetworkTrainer trackml27.nno
+cp ../Networks/NNO0000.TXMLP XMLP27.net
+
